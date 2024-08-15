0197dafc79f6ed10334d75b84c3946a1c935b79e
Cahya Juniar <juniarcahya@gmail.com>
2024-08-15 20:39:54 +0700

    adjusment on style

--- src/components/auth/AuthStepper.tsx
+++ src/components/auth/AuthStepper.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import '../../styles/stepper.css'
 
 interface AuthStepperProps {
     activeStep: number,
