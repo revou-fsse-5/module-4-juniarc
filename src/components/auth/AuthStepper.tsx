b99f2ddd0a3a0a30377b89a6c0b9da7b8a96c709
Cahya Juniar <juniarcahya@gmail.com>
2024-08-15 21:45:20 +0700

    add readme

--- src/components/auth/AuthStepper.tsx
+++ src/components/auth/AuthStepper.tsx
@@ -3,6 +3,8 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 
+import '../../styles/stepper.css'
+
 interface AuthStepperProps {
     activeStep: number,
     steps: string[],
